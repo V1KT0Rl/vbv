d14b43039f5d4b66f5d20be5dc8c955d77a898ba
Viktor <yotciv978@gmail.com>
2026-01-14 21:32:53 +0300

    хук   useLaunchParams();

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -2,15 +2,17 @@ import { Placeholder } from '@telegram-apps/telegram-ui';
 import type { FC } from 'react';
 
 import { Page } from '@/components/Page.tsx';
+import { useLaunchParams } from '@tma.js/sdk-react';
 
 export const IndexPage: FC = () => {
+  const i = useLaunchParams();
   return (
     <Page back={false}>
       <Placeholder
         header="Telegram Mini App"
         description="Приложение готово к работе"
       >
-        {/* Здесь будет ваш контент */}
+      <p>{JSON.stringify(i)}</p>
       </Placeholder>
     </Page>
   );
